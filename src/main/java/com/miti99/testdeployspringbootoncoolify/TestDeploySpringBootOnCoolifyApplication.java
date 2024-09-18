3d02cbe85145856ebbe0a5bfedbecdb83fb38724
tiennm5 <tiennm99@outlook.com>
2024-09-18 16:15:26 +0700

    [Add] init

---
+++ src/main/java/com/miti99/testdeployspringbootoncoolify/TestDeploySpringBootOnCoolifyApplication.java
@@ -0,0 +1,13 @@
+package com.miti99.testdeployspringbootoncoolify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploySpringBootOnCoolifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeploySpringBootOnCoolifyApplication.class, args);
+	}
+
+}
