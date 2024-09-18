ba4e63e1ed7987cfec69bd5c74411c4c09e15b60
tiennm99 <tiennm99@outlook.com>
2024-09-18 16:15:26 +0700

    [Add] init

---
+++ src/test/java/com/miti99/testdeployspringbootoncoolify/TestDeploySpringBootOnCoolifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miti99.testdeployspringbootoncoolify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeploySpringBootOnCoolifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
